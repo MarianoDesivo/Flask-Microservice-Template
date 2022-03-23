6e8cbdd373899aa2a42a999951e4b77002eb8849
Mariano Desivo <79780807+MarianoDesivo@users.noreply.github.com>
2022-03-23 20:50:53 -0300

    Update microservice.py

--- src/microservice.py
+++ src/microservice.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from multiprocessing import Process
 import socket
+from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
+
+
+
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+    return jsonify({'ip': request.remote_addr}), 200
 
 @app.route("/")
 def home():
