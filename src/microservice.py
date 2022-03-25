c8eed2376cf51e7c76f8ac10c3ca0b8ff6a25ef7
Mariano Desivo <79780807+MarianoDesivo@users.noreply.github.com>
2022-03-25 17:41:09 -0300

    Update microservice.py

--- src/microservice.py
+++ src/microservice.py
@@ -1,21 +1,9 @@
 from flask import Flask
-from multiprocessing import Process
-import socket
 from flask import request
 from flask import jsonify
 from flask import make_response
 
 app = Flask(__name__)
-
-
-
-@app.route("/get_my_ip", methods=["GET"])
-def get_my_ip():
-    name = request.args.get("name")
-    if(name):
-        return jsonify({'ip': request.remote_addr, 'greeting': "Welcome, " + name + "!!"}), 200
-    else:
-        return jsonify({'ip': request.remote_addr}), 200
 
 @app.route("/")
 def home():
@@ -28,13 +16,3 @@ def home():
     response.headers["x-hello-world"] = "MD"
     return response
 
-@app.route('/startheavy')
-def startHeavyProcess():
-    heavy_process = Process(target=heavyProcess, daemon=True)
-    heavy_process.start()
-    return 'process has started'
-
-
-def heavyProcess():
-    for x in range(1, 100):
-        print(x)
