6d12feb31415d010e2318df421e40262af1e675e
Mariano Desivo <79780807+MarianoDesivo@users.noreply.github.com>
2022-03-25 17:34:14 -0300

    Update microservice.py

--- src/microservice.py
+++ src/microservice.py
@@ -21,9 +21,9 @@ def get_my_ip():
 def home():
     name = request.args.get("name")
     if(name):
-        response = make_response(jsonify({'ip': request.remote_addr, 'greeting': "Welcome, " + name + "!!"}), 200
+        response = make_response(jsonify({'ip': request.remote_addr, 'greeting': "Welcome, " + name + "!!"}), 200)
     else:
-        response = make_response(jsonify({'ip': request.remote_addr}), 200
+        response = make_response(jsonify({'ip': request.remote_addr}), 200)
     
     response.headers["x-hello-world"] = "MD"
     return response
